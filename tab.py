d47426a6b573c3749b363f8c670af7a64153e72e
Akhilesh Kumar <akhilesh_k@outlook.com>
2017-11-12 18:12:44 +0530

    Add files via upload

--- tab.py
+++ tab.py
@@ -1,9 +1,10 @@
 import webbrowser
 import time
 
-print('Initializing...\n Please Wait.. ')
-
-	tab= 'https://akhilesh-k.github.io/cityiotary'
-	webbrowser.open_new_tab(tab)
+print('Initializing...\n Please Wait..\n')
+print('...........\n')
+print('Checking coordinates.\nOptimizing Routes')
+tab= 'https://akhilesh-k.github.io/CityIoTary'
+webbrowser.open_new_tab(tab)
 	
 time.sleep(7200)
